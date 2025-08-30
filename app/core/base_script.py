efda941daf02cc9e53711fa8dcf86168163fe82f
mehedees <mehedees@live.com>
2025-08-30 23:46:45 +0600

    Added db, compose, init script
    Project is not runnable yet

---
+++ app/core/base_script.py
@@ -0,0 +1,20 @@
+from app.core.db import get_engine, get_session
+
+
+class BaseScript:
+    def __init__(self):
+        print("Initializing Base Script")
+        self.session = None
+        self.engine = None
+
+    def __enter__(self):
+        print("Entering Base Script")
+        self.engine = get_engine()
+        self.session = get_session()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("Exiting Base Script")
+        self.session.close()
+        self.engine.dispose()
+        self.session = None
+        self.engine = None
