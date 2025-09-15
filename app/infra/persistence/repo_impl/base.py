2eec5488c9988dcb9a7772be6f9b07e07e661691
Mehedee Siddique <mehedees@live.com>
2025-09-15 11:12:06 +0000

    Temp commit

--- app/infra/persistence/repo_impl/base.py
+++ app/infra/persistence/repo_impl/base.py
@@ -1,4 +1,3 @@
-import dataclasses
 from collections.abc import Callable
 
 from sqlalchemy.orm import Session
@@ -9,17 +8,4 @@ from app.infra.persistence.models.base import DBModel
 class BaseRepoImpl:
     def __init__(self, model: type[DBModel], session_factory: Callable[..., Session]):
         self.__model = model
-        self.session = session_factory()
-
-    async def create(self, data: dict, entity_type: type[dataclasses]) -> object:
-        obj = self.__model(**data)
-        self.session.add(obj)
-        self.session.flush()
-        print("after flush: ", obj)
-        self.session.refresh(obj)
-        print("after refresh: ", obj)
-
-        return obj.to_dataclass(entity_type)
-
-    async def get(self, query: dict) -> object:
-        pass
+        self.session_factory = session_factory
--- app/infra/persistence/repo_impl/user.py
+++ app/infra/persistence/repo_impl/user.py
@@ -1,5 +1,7 @@
 from dataclasses import asdict
 
+from sqlalchemy import select
+
 from app.domain.users.entities import UserCreateEntity, UserEntity
 from app.infra.persistence.models.user import UserModel
 
@@ -11,7 +13,18 @@ class UserRepoImpl(BaseRepoImpl):
         super().__init__(model=UserModel, **kwargs)
 
     async def create_user(self, user: UserCreateEntity) -> UserEntity:
-        return await self.create(data=asdict(user), entity_type=UserEntity)
+        with self.session_factory() as session:
+            obj = self.__model(**asdict(user))
+            session.add(obj)
+            session.flush()
+            print("after flush: ", obj)
+            session.refresh(obj)
+            print("after refresh: ", obj)
 
-    async def get_user_by_email(self, email: str) -> UserEntity:
-        pass
+        return obj.to_dataclass(UserEntity)
+
+    async def get_user_by_email(self, email: str) -> UserEntity | None:
+        with self.session_factory(read_only=True) as session:
+            stmt = select(UserModel).filter_by(email=email)
+            result = session.execute(stmt).scalars().one_or_none()
+            return result.to_dataclass(UserEntity) if result else None
