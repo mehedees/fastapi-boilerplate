c4506ec1dab2c450d897fdd2e348d14798ea28b7
mehedees <mehedees@live.com>
2025-09-27 21:49:49 +0600

    Create custom API exception based on fastapi http exc

--- app/core/exceptions/__init__.py
+++ app/core/exceptions/__init__.py
@@ -1,3 +1,9 @@
+from .api import APIException
+
+
 class CustomException(Exception):
     def __init__(self, message: str | None = None):
         self.message = message
+
+
+__all__ = ["APIException", "CustomException"]
