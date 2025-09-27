53e71c657c04bd12b4fa6d9f2e6017ba43199135
mehedees <mehedees@live.com>
2025-09-27 21:51:29 +0600

    Moved app bootstrapping methods to core and added exception handler

---
+++ app/core/app.py
@@ -0,0 +1,42 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from fastapi.exception_handlers import http_exception_handler
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+from app.core.container import Container, setup_container
+from app.core.logging import logger
+from app.core.settings import Settings, get_settings
+
+
+class FastAPIApp(FastAPI):
+    container: Container
+
+
+def create_fastapi_app() -> FastAPIApp:
+    settings: Settings = get_settings()
+    container: Container = setup_container(settings)
+
+    app = FastAPIApp(
+        title=settings.APP_NAME,
+        description=settings.APP_NAME,
+        version=settings.APP_VERSION,
+        debug=settings.DEBUG,
+        lifespan=app_lifespan,
+        exception_handlers={StarletteHTTPException: handle_api_exceptions},
+    )
+    app.container = container
+    return app
+
+
+@asynccontextmanager
+async def app_lifespan(app: FastAPIApp):
+    logger.info("Starting app")
+    app.container.init_resources()
+    yield
+    app.container.shutdown_resources()
+    logger.info("Shutting down app")
+
+
+async def handle_api_exceptions(request, exc):
+    return await http_exception_handler(request, exc)
--- app/main.py
+++ app/main.py
@@ -1,40 +1,7 @@
-from contextlib import asynccontextmanager
+from fastapi import status
 
-from fastapi import FastAPI, status
-
-from app.core.container import Container, setup_container
-from app.core.logging import logger
+from app.core.app import create_fastapi_app
 from app.core.schemas.base import APIResponse
-from app.core.settings import Settings, get_settings
-
-
-class FastAPIApp(FastAPI):
-    container: Container
-
-
-def create_fastapi_app() -> FastAPIApp:
-    settings: Settings = get_settings()
-    container: Container = setup_container(settings)
-
-    app = FastAPIApp(
-        title=settings.APP_NAME,
-        description=settings.APP_NAME,
-        version=settings.APP_VERSION,
-        debug=settings.DEBUG,
-        lifespan=app_lifespan,
-    )
-    app.container = container
-    return app
-
-
-@asynccontextmanager
-async def app_lifespan(app: FastAPIApp):
-    logger.info("Starting app")
-    app.container.init_resources()
-    yield
-    app.container.shutdown_resources()
-    logger.info("Shutting down app")
-
 
 app = create_fastapi_app()
 
